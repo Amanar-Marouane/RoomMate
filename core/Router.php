bb0ccded6ccd02b69c3b5fa696f376216dbedb92
Amanar Marouane <marouane.amanar07@gmail.com>
2025-02-11 21:54:17 +0100

    improve the envirement

--- core/Router.php
+++ core/Router.php
@@ -1,11 +1,12 @@
 <?php
 
+namespace core;
+
 use core\Middleware\Middleware;
 
 require_once __DIR__ . "/Functions.php";
 require_once __DIR__ . "/AutoLoader.php";
 
-AutoLoader::autoloader();
 class Router
 {
     private array $routes = [];
---
+++ core/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace core;
+
+require_once __DIR__ . "/AutoLoader.php";
+require_once __DIR__ . "/Functions.php";
+AutoLoader::autoloader();
+
+$router = new Router;
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//nroutiw hna s'il vous plus :>
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//=>
