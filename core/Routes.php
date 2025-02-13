101de7cf8e1a19ceedf88d87293ceb62fa630565
Miloudy bouchra <miloudybouchra01@gmail.com>
2025-02-13 14:31:50 +0100

    Resolve Conflict

--- core/Routes.php
+++ core/Routes.php
@@ -2,12 +2,7 @@
 
 namespace core;
 
-<<<<<<< HEAD
-use app\controllers\UserController;
-use app\controllers\AdminController;
-=======
-use app\controllers\{MessageController, UserController};
->>>>>>> 0d711b2a02c274a4a3df2f6072cd439fbc931233
+use app\controllers\{MessageController, UserController, AdminController};
 
 require_once __DIR__ . "/Functions.php";
 require_once __DIR__ . "/AutoLoader.php";
@@ -19,7 +14,6 @@ $router = new Router;
 //nroutiw hna s'il vous plus :>
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //=>
-<<<<<<< HEAD
 $router->route("get", "profile",  new UserController, "showProfile");
 
 
@@ -30,7 +24,6 @@ $router->route("get", "admin/users", new AdminController, "showViewUser");
 $router->route("post", "admin/users/updatestatus", new AdminController, "updateStatus");
 $router->route("post", "admin/users/deleteuser", new AdminController, "deleteUser");
 $router->route("get", "admin/announces", new AdminController, "showViewAnnounces");
-=======
 $router->route("get", "profile", new UserController, "showProfile");
 $router->route("get", "message/{user_id}", new MessageController, "message");
 $router->route("post", "message/{user_id}", new MessageController, "send");
@@ -40,4 +33,3 @@ $router->route("get", "login", new UserController, "showLogin");
 
 // route for processing login
 $router->route("post", "login", new UserController, "login");
->>>>>>> 0d711b2a02c274a4a3df2f6072cd439fbc931233
